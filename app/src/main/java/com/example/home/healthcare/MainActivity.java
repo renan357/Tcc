b1d6f4302075ad0131b02cd123dfe6e6543626ce
renan357 <30874145+renan357@users.noreply.github.com>
2017-09-05 21:32:44 -0300

    teste
    teste

--- app/src/main/java/com/example/home/healthcare/MainActivity.java
+++ app/src/main/java/com/example/home/healthcare/MainActivity.java
@@ -53,6 +53,7 @@ public class MainActivity extends AppCompatActivity
                         , new MainFragment())
                 .commit();
         context = this;
+        aaa
     }
 
     @Override
